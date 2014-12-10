29e47e111de30d0ca0f544d6e4d8d7dde6fca000
Lebre Marianne <mlebre@bs406-s16-12.insa-lyon.fr>
2014-12-10 17:56:00 +0100

    Test on clear method

--- test_main.cpp
+++ test_main.cpp
@@ -44,24 +44,34 @@ unsigned int String::MAX_SIZE = 100;
 int main(int argc, char* argv[])
 {
   printf("Hello World !\n");
-  String * test = new String();
+  String  test =String();
 
   char mot[8] ="Bonjour";
-  String* test_cstring = new String(mot);
-  printf("size=%d, word= %s\n", test_cstring->GetSize(), test_cstring->GetString());
+  String test_cstring =String(mot);
+  printf("size=%d, word= %s\n", test_cstring.GetSize(), test_cstring.GetString());
 
   //Test for the copy constructor
   String model=String();
   String test_copy=String(model);
 
   //Test on c_str & size methods
-  printf("%s\n",test_cstring->c_str()); 
-  printf("%d\n",test_cstring->Size());
+  printf("%s\n",test_cstring.c_str()); 
+  printf("%d\n",test_cstring.Size());
 
-  printf("%d \n",test_cstring->Capacity());
-  test_cstring->Reserve(10);
-  printf("%d \n",test_cstring->Capacity());
-  return 0;
+  printf("%d \n",test_cstring.Capacity());
+  test_cstring.Reserve(10);
+  printf("%d \n",test_cstring.Capacity());
+
+  //Test on clear method
+  char word[]="Link list";
+  String testo=String(word);
+  printf("%s, size=%d\n", testo.c_str(), testo.Size());
+  testo.clear();
+  printf("%s, size=%d\n", testo.c_str(), testo.Size());
+
+
+
+    return 0;
 }
 
 
