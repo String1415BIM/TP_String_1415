102e520dc8c5de071982ed605df5a48b29116ef4
Debs Noelie <ndebs@bs406-s16-11.insa-lyon.fr>
2014-12-10 09:46:51 +0100

    c-string constructor test

--- test_main.cpp
+++ test_main.cpp
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include "String.h"
 
-
+unsigned int String::MAX_SIZE = 100;
 
 // ===========================================================================
 //                             Include Project Files
@@ -39,6 +39,9 @@
 int main(int argc, char* argv[])
 {
   printf("Hello World !\n");
+  char mot[8] ="Bonjour";
+  String* test = new String(mot);
+
 
   return 0;
 }
