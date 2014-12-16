b5fc11270d852abf975b61fd0234d9c4007f06a7
paulyoungtieyang <pyoung.yty@gmail.com>
2014-12-16 18:12:46 +0100

    Main clearer

--- test_main.cpp
+++ test_main.cpp
@@ -95,26 +95,31 @@ int main(int argc, char* argv[])
   //test AT
   //-------------------------------------------------------------------------------
   
-  printf("At index asked, char= %c \n",test_cstring->at (5));
+  printf("At index asked, char= %c \n\n",test_cstring->at (5));
 
 
+  //-------------------------------------------------------------------------------
+  //test CAPACITY and  RESERVE
+  //-------------------------------------------------------------------------------
+  printf("Capacity \n\n");
 
-  //Test on reserve, capacity, empty methods
-  printf("Capacity test \n");
-  printf("capacite 1 : %d \n",test_cstring->capacity());
+  String* test_reserve = new String();
   
-  printf("Reserve test \n");
+  test_reserve->reserve();
+  printf("Default capacity : %d \n",test_reserve->capacity());
+  
+  printf("capacity 1 : %d \n\n",test_cstring->capacity());
+  printf("Reserve 20 \n");
   test_cstring->reserve(20);
-  printf("capacite 2 : %d \n",test_cstring->capacity());
-  test_cstring->reserve();
-  printf("capacite par defaut : %d \n",test_cstring->capacity());
+  printf("capacite 2 : %d \n\n",test_cstring->capacity());
   
-  printf("size : %d \n", test_cstring->size());
+  //-------------------------------------------------------------------------------
+  //test EMPTY
+  //-------------------------------------------------------------------------------
+  printf("Empty \n\n");
 
-
-  printf("Empty test \n");
-  printf("booleen : %d \n",test_cstring->empty());
-  printf("booleen : %d \n",def_string->empty());
+  printf("booleen of fulled string : %d \n",test_cstring->empty());
+  printf("boolean of empty string : %d \n",test_reserve->empty());
 
 
 
@@ -153,7 +158,14 @@ int main(int argc, char* argv[])
   printf("%s (%d)\n", test_operator2.c_str(), test_operator2.size());
 
 
-
+  // "=" operator with char* type
+  
+  String myString1=String("Bonjour");
+  printf("%s will become ", myString1.c_str() );
+  char* toPut = "Hello";
+  printf("%s. ", toPut );
+  myString1= toPut;
+  printf("Finally : %s\n", myString1.c_str() );
 
 
   //-------------------------------------------------------------------------------
@@ -170,52 +182,31 @@ int main(int argc, char* argv[])
   printf("%s (%d) + !\n", string2.c_str(), string2.size());
 
 
+  // "+" operator with string type
+  String test_op_1 = "Hel";
+  String test_op_2 = "lo";
+  printf("test_op_1:  %s + test_op_2: %s = ", test_op_1.c_str(), test_op_2.c_str() );
+  String returned_Str= test_op_1 + test_op_2; 
+  printf("%s \n", returned_Str.c_str() );
+ 
+
+  //-------------------------------------------------------------------------------
+  //operator "[]"
+  //-------------------------------------------------------------------------------
+  
+  for (int i=0; i<=returned_Str.size(); i++) {
+    printf("%c\n", returned_Str[i] );
+  }
+  printf("Character null : %c\n", test_operator1[12] );
+
+
+
+
+
   //student1
   test_operator2 = test_operator1 + '!';
   test_operator2 = '?' + test_operator1;
   printf("%s (%d, capacity=%d)\n", test_operator2.c_str(), test_operator2.size(), test_operator2.capacity());
-
-  
-
-  //Test on operators of Student 3 
-
-
-  String test_op_1=String("test operat");
-  String test_op_2=String("eur");
-  char char_test[] = " foo";
-    
-    // Test of operator +(string)
-  
-  String returned_Str= test_op_1 + test_op_2; 
-  printf("%s \n", returned_Str.c_str() );
-
-  String returned_chr = returned_Str + char_test;
-  printf("%s \n", returned_chr.c_str() );
-
-  String returned_chr_2= char_test+test_op_1;
-  printf("%s \n", returned_chr_2.c_str() );
-    // Test of operator []
-  for (int i=0; i<=test_operator1.size(); i++) {
-    printf("%c\n", test_operator1[i] );
-  }
-
-  printf("Character null : %c\n", test_operator1[12] );
-
-    
- 
-    // Test of operator =(char*)
-  String myString1=String("Bonjour");
-  printf("%s \n", myString1.c_str() );
-
-
-  char toPut[] = "Hello";
-  printf("%s \n", toPut );
-  myString1= toPut;
-
-  printf("%s\n", myString1.c_str() );
-
-//  printf("Finalement : %s\n", myString1.c_str() );
-
 
 
   return 0;
@@ -229,26 +220,5 @@ int main(int argc, char* argv[])
 // ===========================================================================
 
 
-void test_student_2(void)
-{
-
-
-
-
-
-
-
-
-
-
-  
-  
-
-
-
-
-
-  return 0;
-}
 
 
